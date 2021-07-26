03bbf05969a586f6aa937271ed7d36f537040f13
AlexTaietti <taiettialex@yahoo.it>
2021-07-26 21:37:01 +0100

    🚀 FEAT: add new utility functions
    Add two simple utility functions: one used to scramble arrays
    of numbers and the other one used to convert radians to degress.

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,1 +1,23 @@
 export const clone = (entity: Object | Array<any>) => JSON.parse(JSON.stringify(entity));
+
+export const scramble = (array: Array<any>) => {
+
+   let currentIndex = array.length
+   let randomIndex;
+
+   while (0 !== currentIndex) {
+
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+   }
+
+   return array;
+
+};
+
+export const rad2deg = (degrees: number) => {
+   var pi = Math.PI;
+   return degrees * (pi / 180);
+}
